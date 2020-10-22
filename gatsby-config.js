bfff6994c48e265650d9a14095202c4c9f7895c1
Alice Freda <alicefreda@gmail.com>
2020-10-22 16:48:11 -0700

    add plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,27 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
 export default {
+  pathPrefix: '/pizza',
   siteMetadata: {
     title: `Slicks Slices`,
-    siteUrl: 'http://gatsby.pizza',
-    description: 'The best pizza place in Berkeley!',
+    siteUrl: 'https://gatsby.pizza',
+    description: 'The best pizza place in Hamilton!',
+    twitter: '@slicksSlices',
   },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    {
+      // this is the name of the plugin you are adding
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'p7gziyau',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
 };
