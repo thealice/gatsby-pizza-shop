2c6a665d0c8065a2a2b816b588db91f39f0f0db5
Alice Freda <alicefreda@gmail.com>
2020-10-22 18:20:01 -0700

    display list of pizzas and their toppings on the Pizzas page

--- src/pages/pizzas.js
+++ src/pages/pizzas.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import PizzaList from '../components/PizzaList';
 
 export default function PizzasPage({ data }) {
   const pizzas = data.pizzas.nodes;
   return (
     <>
-      <p>Hey! There are {pizzas.length} Pizzas!</p>
+      <PizzaList pizzas={pizzas} />
     </>
   );
 }
