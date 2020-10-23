b02b50bb8760c9beb1396fc4a16656bcffa7b49f
Alice Freda <alicefreda@gmail.com>
2020-10-22 17:35:59 -0700

    pull in some pizza data to pizzas page

--- src/pages/pizzas.js
+++ src/pages/pizzas.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-export default function PizzasPage() {
-  
+export default function PizzasPage({ data }) {
+  const pizzas = data.pizzas.nodes;
   return (
     <>
-      <p>I am the Pizzas Page!</p>
+      <p>Hey! There are {pizzas.length} Pizzas!</p>
     </>
   );
 }
+export const query = graphql`
+  query PizzaQuery {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        price
+        slug {
+          current
+        }
+        toppings {
+          name
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
